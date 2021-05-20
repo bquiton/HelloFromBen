024f313a563d5c9504fa574c34452252a5a51129
codfish <codfish81@byui.edu>
2021-05-19 21:15:05 -0600

    Added to Ben's project.

--- src/quiton/Main.java
+++ src/quiton/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Ben. \n");
+
+        System.out.println("Hello Ben, my name is Cade.");
     }
 }
